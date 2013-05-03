1b85e9c5d6803fd9bf75642ed07f638977c0978f
Nick <potetoe@gmail.com>
2013-05-03 10:31:18 -0400

    First commit of new review app\!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CorgiApp::Application.config.secret_token = 'bec7cd4ef0e6d0ddec9b731af74196966de1d2e422648dc6432d4f4f6ef54b08c4e0617e5852d02cd14e0dd4587dac5ac8502047904675d32198c747409642f1'
