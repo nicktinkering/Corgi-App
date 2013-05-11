ddd40b1be750d9c826963adaf7e0d4fffb13f08b
Nick <potetoe@gmail.com>
2013-05-11 11:01:33 -0400

    Add footer, improve spec tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,49 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Static Pages" do
 
-	before(:each) do
-		@base_title = "CorgiApp"
+	subject { page }
+
+	shared_examples_for "all static pages with titles" do
+		it { should have_selector('title', :text => full_title(titlesuffix))}
+
 	end
 
 	describe "Home page" do
-		it "should have the default title" do
-			visit home_path
-			page.should have_selector('title', :text => "#{@base_title}")
-		end
+		before { visit root_path }
+
+		let(:titlesuffix) { '' }
+		it_should_behave_like "all static pages with titles"
 	end
 
 	describe "Help page" do
-		it "should have Help title" do
-			visit help_path
-			page.should have_selector('title', :text => "#{@base_title} | Help")
-		end
+
+		before { visit help_path }
+
+		let(:titlesuffix) { "Help" }
+		it_should_behave_like "all static pages with titles"
 	end
 
 	describe "About page" do
-		it "should have the About title" do
-			visit about_path
-			page.should have_selector('title', :text => "#{@base_title} | About")
-		end
+		before { visit about_path }
+
+		let(:titlesuffix) { "About" }
+		it_should_behave_like "all static pages with titles"
 	end
 
 	describe "Contact page" do
-		it "should have the Contact title" do
-			visit contact_path
-			page.should have_selector('title', :text => "#{@base_title} | Contact")
-		end
+		before { visit contact_path }
+
+		let(:titlesuffix) { "Contact" }
+		it_should_behave_like "all static pages with titles"
 	end
+
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About')
+	end
+
 end
